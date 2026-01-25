950b0db2d48af27a3adbaac85603579a7831d6dc
Nuno Capitão <nuno_capitao_18@hotmail.com>
2026-01-25 12:03:27 +0000

    chore: setup inits

--- src/molli_pinn_node_lstm/node_lstm/__init__.py
+++ src/molli_pinn_node_lstm/node_lstm/__init__.py
@@ -1,1 +1,3 @@
 from molli_pinn_node_lstm.node_lstm.model import MOLLINeuralODELSTM
+
+__all__ = ["MOLLINeuralODELSTM"]
---
+++ src/molli_pinn_node_lstm/training/__init__.py
@@ -0,0 +1,4 @@
+from molli_pinn_node_lstm.training.bloch_loss import SignalRecoveryLoss, PINNLoss
+from molli_pinn_node_lstm.training.trainer import Trainer, TrainerConfig
+
+__all__ = ["SignalRecoveryLoss", "PINNLoss", "Trainer", "TrainerConfig"]
